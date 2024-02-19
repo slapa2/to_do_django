a351f97edcce71782f85e096b691ba6e0460eac4
slapa2 <slawomirpawlowski@gmail.com>
2024-02-19 19:39:58 +0100

    added task model

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=256)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now=True)
+    done = models.BooleanField(default=False)
