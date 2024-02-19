e3bb8589fda29a1419d42c27a68f95651919308b
slapa2 <slawomirpawlowski@gmail.com>
2024-02-19 19:48:09 +0100

    added clas based view instead of function based view

--- to_do/views.py
+++ to_do/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 
-def home(request):
-    context = {}
-    return render(request, 'to_do/index.html', context)
+class Home(View):
+
+    def get(self, request):
+        context = {}
+        return render(request, 'to_do/index.html', context)
