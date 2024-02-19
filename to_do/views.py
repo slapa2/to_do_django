bc566a30684e174bb2e8c39a1a200106d8afd71c
slapa2 <slawomirpawlowski@gmail.com>
2024-02-19 20:03:38 +0100

    added task list to home page

--- to_do/views.py
+++ to_do/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Task
 
 
 class Home(View):
 
     def get(self, request):
-        context = {}
+        qs = Task.objects.all()
+        context = {
+            'qs': qs,
+        }
         return render(request, 'to_do/index.html', context)
