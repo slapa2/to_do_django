767dad158b2db5a9df299dcdc7d4489a5621c9ce
slapa2 <slawomirpawlowski@gmail.com>
2024-02-19 20:05:57 +0100

    fixed auto now add in task model

--- to_do/models.py
+++ to_do/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=256)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     done = models.BooleanField(default=False)
