816cf267534892285eff0f20f89556650e118210
slapa2 <slawomirpawlowski@gmail.com>
2024-03-03 23:43:24 +0100

    added create view

--- to_do/urls.py
+++ to_do/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.TaskListView.as_view(), name='task_list_view'),
+    path('', views.TaskListView.as_view(), name='task-list-view'),
+    path("add/", views.TaskCreateView.as_view(), name="task-add"),
 ]
--- to_do/views.py
+++ to_do/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Task
 
 
 class TaskListView(ListView):
     model = Task
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['name']
 
