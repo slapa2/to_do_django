1295f9d55f06c888672b86c00742b3bb5f4bb010
slapa2 <slawomirpawlowski@gmail.com>
2024-03-08 21:56:19 +0100

    added owner to Task model

--- to_do/models.py
+++ to_do/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth import get_user_model
+
 
 class Task(models.Model):
     name = models.CharField(max_length=256)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     done = models.BooleanField(default=False)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
